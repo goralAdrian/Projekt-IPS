4faae5c50e848b80ac855298037cf55a912b3ff0
goralAdrian <37652885+goralAdrian@users.noreply.github.com>
2019-12-10 12:12:24 +0100

    Add files via upload

---
+++ front-end/src/core/Book.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Book = () => (
+  <div className="jumbotron">
+  <h2>Księgarnia internetowa</h2>
+  </div>
+  <div className="jumbotron">
+  
+  </div>
+);
+
+export default Book;
