700b2b7e7b49536d1a52a1fd9d96a69f65c42207
goralAdrian <37652885+goralAdrian@users.noreply.github.com>
2020-01-04 23:24:45 +0100

    Add files via upload

---
+++ routes/payNow.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const common = require('../lib/common');
+const { restrict, checkAccess } = require('../lib/auth');
+const escape = require('html-entities').AllHtmlEntities;
+const colors = require('colors');
+const bcrypt = require('bcryptjs');
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const mime = require('mime-type/with-db');
+const ObjectId = require('mongodb').ObjectID;
+const router = express.Router();
+
+
+
+
+
+
+router.post('/payNow', async (req, res) => {
+
+});
+
+module.exports = router;
